6cd159200b48fe77cd34e38e81c90e8c8424ee15
carla <carla@BOOK>
2023-12-23 15:32:39 -0300

    Add project files.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BurcarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Data/SistemaDeTarefasDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class SistemaDeTarefasDBContext : DbContext
+    {
+        /*O construtor aceita opções de configuração para o contexto do banco de dados.
+        Ele chama o construtor da classe base (DbContext) passando essas opções, garantindo que a inicialização básica do contexto do banco de dados seja realizada 
+        corretamente.*/
+        public SistemaDeTarefasDBContext(DbContextOptions<SistemaDeTarefasDBContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
