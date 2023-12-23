89a469c811dea0650631bf7f8e9763daf9f6df89
carlakremer <carlakremerf@gmail.com>
2023-12-23 17:28:11 -0300

    Configurando BD pelo Entity Framework e criado métodos para o usuário

--- Data/SistemaDeTarefasDBContext.cs
+++ Data/SistemaDeTarefasDBContext.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
     public class SistemaDeTarefasDBContext : DbContext
     {
-        /*O construtor aceita opções de configuração para o contexto do banco de dados.
+        /*
+         Entity Framework: facilita trabalhar com bd
+        ORM vai fzer toda estrutura de entidade, e depois iremos criar bd e tabelas atravéz do código
+        O construtor aceita opções de configuração para o contexto do banco de dados.
         Ele chama o construtor da classe base (DbContext) passando essas opções, garantindo que a inicialização básica do contexto do banco de dados seja realizada 
-        corretamente.*/
+        corretamente.
+        
+        configurando tabelas:
+         */
         public SistemaDeTarefasDBContext(DbContextOptions<SistemaDeTarefasDBContext> options)
             :base(options)
         {
             
         }
+        
+        //Representa a tabela Usuarios:
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        //Representa a tabela Tarefas:
+        public DbSet<TarefaModel> Tarefas{ get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
     }
 }
---
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Repositorios.Interfaces
+{
+    // Vai ser responsavel por ter os contratos de usuario: buscar, remover, adicionar
+    public interface IUsuarioRepositorio
+    {
+        //Task: assincrono
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+
+    }
+}
